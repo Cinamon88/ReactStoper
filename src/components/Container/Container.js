bbad6777b26c505ada99d5afc96a4a7825557c77
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-10-30 15:46:36 +0000

    add Timer component

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,7 +1,14 @@
 import styles from './Container.module.scss';
 import Button from '../Button/Button';
+import {useState} from 'react';
 
 function Container () {
+    const [start, setStart] = useState(false);
+    const [stop, setStop] = useState(true);
+    const [clock, setClock] = useState(0);
+
+    
+
     return (
         <div className={styles.container}>
             <div className={styles.button}>
