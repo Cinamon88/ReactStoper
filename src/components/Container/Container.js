6d55640a4fd1713f274dec148e485394aab51a6a
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-10-30 16:15:43 +0000

    add func Container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,20 +1,50 @@
 import styles from './Container.module.scss';
 import Button from '../Button/Button';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import Timer from '../Timer/Timer';
 
 function Container () {
     const [start, setStart] = useState(false);
     const [stop, setStop] = useState(true);
     const [clock, setClock] = useState(0);
 
-    
+    useEffect (() => {
+        let interval = null;
+
+        if(start && stop === false) {
+            interval = setInterval(() => {
+                setClock((clock) => clock + 10);
+            }, 10);
+        } else {
+            clearInterval(interval);
+        }
+        return () => {
+            clearInterval(interval);
+        };
+    }, [start, stop]);
+
+    const Start = () => {
+        setStart(true);
+        setStop(false);
+    }
+
+    const Stop = () => {
+        setStart(false);
+        setStop(true);
+    }
+
+    const Reset = () => {
+        setStart(false);
+        setClock(0);
+    };
 
     return (
         <div className={styles.container}>
+            <Timer time={clock} />
             <div className={styles.button}>
-                <Button>Start</Button>
-                <Button>Stop</Button>
-                <Button>Reset</Button>
+                <Button onClick={Start}>Start</Button>
+                <Button onClick={Stop}>Stop</Button>
+                <Button onClick={Reset}>Reset</Button>
             </div>
         </div>
     );
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -3,10 +3,13 @@ import styles from "./Timer.module.scss";
 const Timer = (props) => {
   return (
     <div className={styles.timer}>
-      <span className="digits">
+      <span className="digits hours">
+        {("0" + Math.floor((props.time / 3600000) % 60)).slice(-2)}:
+      </span>
+      <span className="digits minutes">
         {("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:
       </span>
-      <span className="digits">
+      <span className="digits seconds">
         {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}:
       </span>
       <span className="digits mili-sec">
